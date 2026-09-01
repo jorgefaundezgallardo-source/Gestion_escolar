b55f2fe0592dd6098478d301a11d8d1713f81a9e
Jorge Faundez <jorgefaundezgallardo@gmail.com>
2026-09-01 02:07:28 -0400

    Permisos de acceso: Info publica para docentes y privada para alumnos

--- api/views.py
+++ api/views.py
@@ -1,15 +1,22 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from .serializer import DocenteSerializer, AlumnoSerializer, RegistroAcademicoSerializer
 from .models import Docente, Alumno, RegistroAcademico
 
 class DocenteViewSet(viewsets.ModelViewSet):
-    queryset = Docente.objects.all() 
-    serializer_class = DocenteSerializer 
+    queryset = Docente.objects.all()
+    serializer_class = DocenteSerializer
+    # IsAuthenticatedOrReadOnly: Cualquiera puede ver a los docentes (público), 
+    # pero solo usuarios autenticados pueden crear o editar.
+    permission_classes = [IsAuthenticatedOrReadOnly] 
 
 class AlumnoViewSet(viewsets.ModelViewSet):
-    queryset = Alumno.objects.all() 
+    queryset = Alumno.objects.all()
     serializer_class = AlumnoSerializer
+    # IsAuthenticated: Información 100% privada. Requiere login obligatorio.
+    permission_classes = [IsAuthenticated] 
 
 class RegistroAcademicoViewSet(viewsets.ModelViewSet):
-    queryset = RegistroAcademico.objects.all() 
-    serializer_class = RegistroAcademicoSerializer 
+    queryset = RegistroAcademico.objects.all()
+    serializer_class = RegistroAcademicoSerializer
+    permission_classes = [IsAuthenticated]
