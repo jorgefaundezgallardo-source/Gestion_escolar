5d8e1c5dfdb082802c4be03498dcb3357ab596e8
Jorge Faundez <jorgefaundezgallardo@gmail.com>
2026-09-02 19:28:51 -0400

    Agregada pagina de inicio

--- drf/urls.py
+++ drf/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')) 
+    path('api/', include('api.urls')),
+    # Esta línea conecta la ruta vacía con tu nueva pantalla de bienvenida
+    path('', TemplateView.as_view(template_name='bienvenida.html'), name='inicio'), 
 ]
