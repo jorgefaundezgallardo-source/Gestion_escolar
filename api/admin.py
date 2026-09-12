0e22b50511c40bc5d6338b8aeeb2f4e03ffccd04
Jorge Faundez <jorgefaundezgallardo@gmail.com>
2026-09-11 23:48:49 -0300

    Registro basico de modelos en admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Alumno, Docente, RegistroAcademico
 
-# Register your models here.
+# Registramos los modelos de forma directa para habilitar el CRUD
+admin.site.register(Alumno)
+admin.site.register(Docente)
+admin.site.register(RegistroAcademico)
